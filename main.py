adba8b67c63017ef90eb5e1d25a5753d9713f2d6
Noursaqer <nooroned@gmail.com>
2022-09-23 02:44:48 -0700

    updated code and made it shorter

---
+++ main.py
@@ -0,0 +1,9 @@
+num1 = int(input("enter number 1"))
+num2 = int(input("enter number 2"))
+num3 = int(input("enter number 3"))
+num4 = int(input("enter number 4"))
+num5 = int(input("enter number 5"))
+
+alo = [num1, num2, num3, num4, num5]
+result = max(alo)
+print(result)
